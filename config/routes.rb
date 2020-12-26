42c46852dede23b69c46d04d0341f4a93427bdb9
mino <oil_chen@cycu.org.tw>
2020-12-26 08:45:01 +0800

    home/page fix finish

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'pages#home'
+
+  
+
   devise_for :users
   devise_scope :user do
     get '/users/sign_out' => 'devise/sessions#destroy'
