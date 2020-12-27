276f1bd78c2e3a487238c052206f3afc68e91b0a
mino <oil_chen@cycu.org.tw>
2020-12-27 15:14:51 +0800

    img input ok

---
+++ app/javascript/packs/homepage.js
@@ -0,0 +1,10 @@
+require("@rails/ujs").start()
+require("turbolinks").start()
+require("@rails/activestorage").start()
+require("channels")
+import "tailwindcss/dist/tailwind.css"  
+require("styles")
+
+
+
+import '@fortawesome/fontawesome-free/css/fontawesome.min.css'
