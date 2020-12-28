4e66bb3a665b973a603e37028c396fbf9d9e81c2
mino <oil_chen@cycu.org.tw>
2020-12-28 14:04:00 +0800

    yarn add postcss

--- app/javascript/styles/index.js
+++ app/javascript/styles/index.js
@@ -1,11 +1,12 @@
 // import "tailwindcss/base";
 // import "tailwindcss/components";
 // import "./reset.scss";
-import "./grid.scss";
+// import "./grid.scss";
 import "./navbar.scss";
 // import "tailwindcss/utilities";
 import "./page.scss";
 import "./devise.scss";
+import "./component.scss";
 import "tailwindcss/dist/base.css"
 
 
