0bf6383b6c8f9031f8b572657e1dd5b663506ac2
mino <oil_chen@cycu.org.tw>
2020-12-30 11:33:32 +0800

    change br

--- app/javascript/styles/index.js
+++ app/javascript/styles/index.js
@@ -1,7 +1,5 @@
 // import "tailwindcss/base";
 // import "tailwindcss/components";
-// import "./reset.scss";
-// import "./grid.scss";
 import "./navbar.scss";
 // import "tailwindcss/utilities";
 import "./home.scss";
