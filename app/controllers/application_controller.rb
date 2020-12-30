ec02ee5005d7a98027da4d259668db36d2279b20
mino <oil_chen@cycu.org.tw>
2020-12-30 11:20:28 +0800

    fix home_page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,10 @@ class ApplicationController < ActionController::Base
   # before_action :authenticate_user!先不要
 
   protected
+  def after_sign_in_path_for(resource)
+    # 登入後指定頁面
+    querys_path
+  end
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
     devise_parameter_sanitizer.permit(:account_update, keys: [:nickname])
