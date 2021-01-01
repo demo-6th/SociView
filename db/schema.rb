2843518e02095e4d9568f49dec2e280cecc08b53
DanteChengOUO <73519611+DanteChengOUO@users.noreply.github.com>
2021-01-01 21:10:54 +0800

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 2020_12_30_053748) do
-=======
 ActiveRecord::Schema.define(version: 2021_01_01_120759) do
->>>>>>> finsih and setbackpoint
 
-  # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "boards", force: :cascade do |t|
