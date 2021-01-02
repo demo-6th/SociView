d925f7461ee1e198814c5fbfb2304e425f7fc122
Fish Kuo <fishkuo0101@gmail.com>
2021-01-02 22:25:35 +0800

    edit subprocess

--- lib/tasks/Crawler/Dcard/packages.py
+++ lib/tasks/Crawler/Dcard/packages.py
@@ -4,9 +4,10 @@ import subprocess
 
 print("=====[你正在使用的python是"+str(sys.executable)+"]=====")
 # implement pip as a subprocess:
+subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'requests'])
 subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'pandas'])
 subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'Counter'])
 subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'ckip-segmenter'])
-subprocess.check_call([sys.executable, '-m', 'pip3', 'install', 'tqdm'])
+subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'tqdm'])
 
 print("=====[successfully installed python packages.]=====")
