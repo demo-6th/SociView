a4ab6166cc817e7143e3999a84bf7493b4431ed0
dante <dantecherng@hotmail.com.tw>
2021-01-02 21:42:35 +0800

    add tqdm

--- lib/tasks/Crawler/Dcard/packages.py
+++ lib/tasks/Crawler/Dcard/packages.py
@@ -5,11 +5,9 @@ import subprocess
 
 print("=====[你正在使用的python是"+str(sys.executable)+"]=====")
 # implement pip as a subprocess:
-subprocess.check_call([sys.executable, '-m', 'pip', 'install', 
-'pandas'])
-subprocess.check_call([sys.executable, '-m', 'pip', 'install', 
-'Counter'])
-subprocess.check_call([sys.executable, '-m', 'pip', 'install', 
-'ckip-segmenter'])
+subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'pandas'])
+subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'Counter'])
+subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'ckip-segmenter'])
+subprocess.check_call([sys.executable, '-m', 'pip3', 'install', 'tqdm'])
 
 print("=====[successfully installed python packages.]=====")
