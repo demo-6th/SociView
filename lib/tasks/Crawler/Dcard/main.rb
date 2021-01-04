1a6822cd8dd81424e4df32cf333e779c87eeb9f4
dante <dantecherng@hotmail.com.tw>
2021-01-04 14:38:44 +0800

    merge craeler files

--- lib/tasks/Crawler/Dcard/main.rb
+++ lib/tasks/Crawler/Dcard/main.rb
@@ -3,10 +3,7 @@ require "json"
 require "csv"
 require "fileutils"
 #
-require("#{Rails.root}/lib/tasks/Crawler/Dcard/get_forums.rb")
-require("#{Rails.root}/lib/tasks/Crawler/Dcard/get_post_id.rb")
-require("#{Rails.root}/lib/tasks/Crawler/Dcard/get_post_content.rb")
-require("#{Rails.root}/lib/tasks/Crawler/Dcard/get_post_comment.rb")
+require("#{Rails.root}/lib/tasks/Crawler/Dcard/get_post.rb")
 require("#{Rails.root}/lib/tasks/Crawler/Dcard/process_files.rb")
 
 def loop_crawler
@@ -28,19 +25,8 @@ def loop_crawler
     get_post_id(board, sleep_every, sleep_time, prev_day)
     get_post_content(sleep_every, sleep_time)
     get_post_comment(sleep_every, sleep_time)
-    # data cleaning process
-    cleaned_data = `python3 lib/tasks/Crawler/Dcard/data_cleaning.py params`
-    puts(cleaned_data)
+    `python3 lib/tasks/Crawler/Dcard/data_cleaning.py params`
     csv_to_psql()
     mv_files(table_title)
   end
-end
-
-def dcard
-  # python packages
-  pkg = `python3 lib/tasks/Crawler/Dcard/packages.py params`
-  puts(pkg)
-  get_forums()
-  folder_name()
-  loop_crawler()
 end
