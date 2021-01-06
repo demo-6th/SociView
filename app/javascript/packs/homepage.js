7a9689bc5450a7e992a390415df196197cd27aec
mino <oil_chen@cycu.org.tw>
2021-01-06 11:09:55 +0800

    delete not used code

--- app/javascript/packs/homepage.js
+++ app/javascript/packs/homepage.js
@@ -1,10 +1,3 @@
-require("@rails/ujs").start()
-require("turbolinks").start()
-require("@rails/activestorage").start()
-require("channels")
-import "styles" 
-import "tailwindcss/tailwind.css"  
 
-// import "tailwindcss/dist/base.css"
 
 
