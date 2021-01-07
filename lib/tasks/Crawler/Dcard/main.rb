93eb4c6c3b53324ffff62d624337fd2d28eb10ce
Fish Kuo <fishkuo0101@gmail.com>
2021-01-07 13:18:30 +0800

    reduce calls of board update to once per rake

--- lib/tasks/Crawler/Dcard/main.rb
+++ lib/tasks/Crawler/Dcard/main.rb
@@ -10,7 +10,8 @@ def loop_crawler
   all_boards = CSV.parse(File.read("#{Rails.root}/data/forums.csv"), headers: false)
 
   # 0.upto = 從第一個版開始
-  7.upto(all_boards.count - 1) do |board|
+  # 7.upto(all_boards.count - 1) do |board|
+  7.upto(9) do |board|
     #first.first待改
     table_title = all_boards["#{board}".to_i.."#{board}".to_i].first.first
 
@@ -30,4 +31,5 @@ def loop_crawler
     csv_to_psql()
     mv_files(table_title)
   end
+  update_boards()
 end
