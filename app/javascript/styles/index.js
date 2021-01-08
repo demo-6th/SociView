c20c4cd6fccd233b119969152a9832ec5f250c39
mino <oil_chen@cycu.org.tw>
2021-01-08 15:45:43 +0800

    delete vanillajs-datepicker

--- app/javascript/styles/index.js
+++ app/javascript/styles/index.js
@@ -6,6 +6,6 @@ import "./querys.scss";
 import '@fortawesome/fontawesome-free/css/all.min.css'
 import '@fortawesome/fontawesome-free/js/all.js'
 import "tailwindcss/dist/tailwind.css"  
-import "vanillajs-datepicker"
+
 
 
