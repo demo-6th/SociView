98f4319706e9ee7bcb78c135ce2ed71c0fe6b1e0
mino <oil_chen@cycu.org.tw>
2021-01-08 15:24:51 +0800

    use flatpickr

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,33 +2,14 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
-
-require("@rails/ujs").start();
-require("turbolinks").start();
-require("@rails/activestorage").start();
-require("channels");
-require("styles");
-
 const images = require.context('../images', true)
 const imagePath = (name) => images(name, true)
 
-//   function addItemBtn(){
-//     console.log("hello")
-//     const list_page = document.querySelector('.search_list')
-//     const search_item = document.createElement('div')
-//     search_item.classList.add('search_data')
-//     search_item.innerHTML = `<div class="container">
-//                               <div class="w-full px-2">
-//                                 <div class="search_all bg-red-100 px-5 border-solid border-gray-800 border-2 leading-6">
-//                                   <span>查詢結果：189筆</span>
-//                                   <h3>查詢主題：空氣清淨機</h3>
-//                                   <p>資料來源：Dcard</p>
-//                                 </div>
-//                               </div>
-//                             </div>`
-//     if (document.querySelector(".search_data") !== null){
-//       return
-//     }
-//     list_page.appendChild(search_item)
-//   }
-// })
+require("@rails/ujs").start()
+require("turbolinks").start()
+require("@rails/activestorage").start()
+require("channels")
+require("styles")
+require("script")
+import flatpickr from "flatpickr"
+import "flatpickr/dist/flatpickr.min.css"
