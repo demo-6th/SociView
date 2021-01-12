d6c3250690b793379be1e54a8f9ffa927a16170c
mino <oil_chen@cycu.org.tw>
2021-01-12 15:50:25 +0800

    log_up and log_in page fix

--- app/javascript/styles/index.js
+++ app/javascript/styles/index.js
@@ -3,6 +3,7 @@ import "./home.scss";
 import "./devise.scss";
 import "./component.scss";
 import "./querys.scss";
+import "./footer.scss";
 import '@fortawesome/fontawesome-free/css/all.min.css'
 import '@fortawesome/fontawesome-free/js/all.js'
 import "tailwindcss/dist/tailwind.css"  
