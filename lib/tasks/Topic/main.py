5e772a1844574e48c8bbdb75bc7d44fe9a68314a
Fish Kuo <fishkuo0101@gmail.com>
2021-01-15 19:09:14 +0800

    fix nan displayed in the pyLDAvis chart

--- lib/tasks/Topic/main.py
+++ lib/tasks/Topic/main.py
@@ -8,8 +8,7 @@ from sklearn.feature_extraction.text import CountVectorizer
 
 data = pd.read_csv("data/topic_text.csv",names=["id", "token"])
 count_vec = CountVectorizer(max_df = 0.85, min_df = 2) 
-data["token"].fillna(" ")
-data_cv = count_vec.fit_transform(data["token"].values.astype('U')) 
+data_cv = count_vec.fit_transform(data["token"].dropna().values.astype('U')) 
 data_dtm = pd.DataFrame(data_cv.toarray(), columns = count_vec.get_feature_names())
 tdm = data_dtm.transpose()
 sparse_counts = scipy.sparse.csr_matrix(tdm)
@@ -21,5 +20,4 @@ d.id2token = id2word
 d.token2id = word2id
 lda = models.LdaModel(corpus = corpus, id2word = id2word, num_topics = 10, passes= 5, minimum_probability=0.01,random_state=123)
 lda_vis = pyLDAvis.gensim.prepare(lda, corpus, d)
-# pyLDAvis.display(lda_vis)
 pyLDAvis.save_html(lda_vis,"app/views/shared/_ldavis.html")
