84f88268ba76c5b167d1905b8e828d65aab588e9
mino <oil_chen@cycu.org.tw>
2021-01-16 14:41:10 +0800

    fix home_page console error

--- app/javascript/packs/homepage.js
+++ app/javascript/packs/homepage.js
@@ -1,2 +1,12 @@
+const images = require.context("../images", true);
+const imagePath = (name) => images(name, true);
 
-// import "tailwindcss/dist/base.css"
+require("@rails/ujs").start();
+require("turbolinks").start();
+require("@rails/activestorage").start();
+require("channels");
+require("styles");
+require("script");
+require("chart.js");
+require("flatpickr")
+import "flatpickr/dist/flatpickr.min.css";
