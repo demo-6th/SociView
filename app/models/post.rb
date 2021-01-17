95e7b4fe72a070bec56e7f8aae33729f569f6368
Ryan Lee <ryanlee.coding@gmail.com>
2021-01-17 22:08:43 +0800

    post search data

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,16 @@
 class Post < ApplicationRecord
   belongs_to :board, foreign_key: "alias", primary_key: "alias"
   has_many :comments, foreign_key: "pid", primary_key: "pid"
+  searchkick
+  scope :search_import, -> { includes(:board, :comments) }
+  def search_data
+    {
+       title: title,
+       content: content,
+       created_at: created_at,
+       board_name: board.name,
+       comment_content: comments.map(&:content)
+    }
+  end
 end
 
