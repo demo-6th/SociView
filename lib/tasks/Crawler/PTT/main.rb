818e646e3a6ef29fddadcb9c36f569e5443195c0
Fish Kuo <fishkuo0101@gmail.com>
2021-01-18 17:59:01 +0800

    edit

--- lib/tasks/Crawler/PTT/main.rb
+++ lib/tasks/Crawler/PTT/main.rb
@@ -6,3 +6,6 @@ require("#{Rails.root}/lib/tasks/Crawler/PTT/get_board_url.rb")
 require("#{Rails.root}/lib/tasks/Crawler/PTT/get_post_url.rb")
 require("#{Rails.root}/lib/tasks/Crawler/PTT/get_post_content.rb")
 
+# get_board()
+# get_post_url(["1/16"]) # 設定日期，要抓的日期都要寫出來
+# get_post_content()
