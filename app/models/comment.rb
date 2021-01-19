df04fbf71a7904d4b56cbf54ee634c295137adaf
Ryan Lee <ryanlee.coding@gmail.com>
2021-01-20 04:26:58 +0800

    ransack

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,12 +1,3 @@
 class Comment < ApplicationRecord
   belongs_to :post, foreign_key: "pid", primary_key: "pid"
-  searchkick
-  scope :search_import, -> { includes(:post) }
-  def search_data
-    {
-       content: content,
-       created_at: created_at,
-       post: post
-    }
-  end
 end
