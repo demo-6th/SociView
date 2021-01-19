7424d2960508b5a460f8fa6497adaf2df7f3d264
mino <oil_chen@cycu.org.tw>
2021-01-19 16:36:56 +0800

    fix label name

--- app/javascript/script/index.js
+++ app/javascript/script/index.js
@@ -1,4 +1,9 @@
 import "./checkbox.js";
 import "./datepicker.js";
 import "./sentiment.js";
+<<<<<<< HEAD
 import "./volume_ajax.js";
+=======
+import "./volume.js";
+// import "./volume_ajax.js";
+>>>>>>> 300a0c0 (fix label name)
