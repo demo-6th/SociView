424f6d9a9ddc664befb047d14699248e6731a7ee
Ryan Lee <ryanlee.coding@gmail.com>
2021-01-20 00:10:11 +0800

    searchkick closed

--- app/controllers/queries_controller.rb
+++ app/controllers/queries_controller.rb
@@ -13,7 +13,7 @@ class QueriesController < ApplicationController
     @end = params[:user][:end].to_s
     @type = [params[:post], params[:comment]].delete_if { |x| x == nil }
     # query = @theme.presence
-    query = ["五倍紅寶石"]
+    query = ["蝙蝠俠"]
     if params[:dcard] && params[:ptt]
       @source_id = nil
     elsif params[:dcard] && !params[:ptt]
@@ -51,7 +51,7 @@ class QueriesController < ApplicationController
       @comment_tatal = @comment_tatal.select { |comment| comment.post.board.source_id == @source_id } if @source_id
       @count = comment_count
     end
-    total = @post + @comment_tatal
+    # total = @post + @comment_tatal
   end
 
   def sentiment; end
@@ -213,13 +213,16 @@ class QueriesController < ApplicationController
   def diffusion; end
 
   private
+  def search_post_only(query)
+    @posts = Post.search query,fields: [:title, :content], match: :phrase , operator: "or",where: {created_at: {gte: @start, lte: @end}},order: {created_at: {order: "asc"}}
+  end
   # def search_post_only(query)
   #   @posts = Post.search query,fields: [:title, :content], misspellings: false, operator: "or",where: {created_at: {gte: @start, lte: @end}},order: {created_at: {order: "asc"}}
   # end
 
-  def search_post_only(query)
-    @posts = Post.ransack(title_content_cont_any: %w[5xruby camp]).result
-  end
+  # def search_post_only(query)
+  #   @posts = Post.ransack(title_content_cont_any: %w[5xruby camp]).result
+  # end
 
   def search_comment_only(query)
     @comments = Comment.search query,fields: [:content], misspellings: false, operator: "or",where: {created_at: {gte: @start, lte: @end}}
