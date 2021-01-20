3b6cdda0c6b7e601834beb355a9b37be58a5a253
mino <oil_chen@cycu.org.tw>
2021-01-20 18:11:17 +0800

    volume_pic show

--- app/javascript/script/index.js
+++ app/javascript/script/index.js
@@ -1,5 +1,5 @@
 import "./checkbox.js";
 import "./datepicker.js";
 import "./sentiment.js";
-import "./volume.js";
+// import "./volume.js";
 import "./volume_ajax.js";
--- app/javascript/script/volume_ajax.js
+++ app/javascript/script/volume_ajax.js
@@ -1,3 +1,5 @@
+import volume_chart from "./volume.js";
+
 const axios = require('axios')
 document.addEventListener("turbolinks:load", () =>{
   const volume_btn = document.querySelector(".volume #analy")
@@ -8,9 +10,16 @@ document.addEventListener("turbolinks:load", () =>{
       const token = document.querySelector('[name=csrf-token]').content
       axios.defaults.headers.common['X-CSRF-TOKEN'] = token
 
-      axios.post('/queries/diffusion', { token })
+      var formEl = document.forms.query_ajax;
+      var formData = new FormData(formEl);
+      var object = {};
+      formData.forEach(function (value, key) {
+        object[key] = value;
+      });
+
+      axios.post('/queries/volume', object)
         .then(function(resp){
-          console.log(resp);
+           const { count1, count2, count3, theme, start, end, source, type, gon } = resp.data
            const users_query_page = document.querySelector(".search_list")
            users_query_page.innerHTML =`<div class="container">
            <div class="row">
@@ -23,7 +32,19 @@ document.addEventListener("turbolinks:load", () =>{
                <p id="type">資料種類：${type}</p>
              </div>
            </div>
+         </div>
+         <div class="container">
+           <canvas id="volumePieChart" width="960px" height="400px"></canvas>
+         </div>
+         <br>
+         <div class="container">
+           <canvas id="volumeBarChart" width="960px" height="400px"></canvas>
+         </div>
+         <br>
+         <div class="container">
+           <canvas id="volumeLineChart" width="960px" height="400px"></canvas>
          </div>`
+         volume_chart(gon)
         })
         .catch(function(err){
           console.log(err);
