48e325e42647d2164ca5dd69ef69be27f2b9090a
Fish Kuo <fishkuo0101@gmail.com>
2021-01-20 23:15:22 +0800

    dcard pos tagging process

--- lib/tasks/Crawler/Dcard/main.rb
+++ lib/tasks/Crawler/Dcard/main.rb
@@ -7,29 +7,31 @@ require("#{Rails.root}/lib/tasks/Crawler/Dcard/get_post.rb")
 require("#{Rails.root}/lib/tasks/Crawler/Dcard/process_files.rb")
 
 def loop_crawler
-  all_boards = CSV.parse(File.read("#{Rails.root}/data/forums.csv"), headers: false)
-
-  # 0.upto = 從第一個版開始
-  # 0.upto(all_boards.count - 1) do |board|
-  0.upto(5) do |board|
-    #first.first待改
-    table_title = all_boards["#{board}".to_i.."#{board}".to_i].first.first
-
-    # n 天前的資料
-    prev_day = 20
-
-    # 每 n 筆資料暫停 / 隨機請參考rand(n..m)
-    sleep_every = 50
-
-    # 暫停時休息秒數 / 隨機請參考rand(n..m)
-    sleep_time = 3
-
-    get_post_id(board, sleep_every, sleep_time, prev_day)
-    get_post_content(sleep_every, sleep_time)
-    get_post_comment(sleep_every, sleep_time)
-    `python3 lib/tasks/Crawler/Dcard/data_cleaning.py`
-    csv_to_psql()
-    mv_files(table_title)
-  end
+  csv_to_psql()
+  # mv_files(table_title)
   update_boards()
+  # all_boards = CSV.parse(File.read("#{Rails.root}/data/forums.csv"), headers: false)
+
+  # # 0.upto = 從第一個版開始
+  # 0.upto(all_boards.count - 1) do |board|
+  #   # #first.first待改
+  #   table_title = all_boards["#{board}".to_i.."#{board}".to_i].first.first
+
+  #   # n 天前的資料
+  #   prev_day = 1
+
+  #   # 每 n 筆資料暫停 / 隨機請參考rand(n..m)
+  #   sleep_every = 50
+
+  #   # 暫停時休息秒數 / 隨機請參考rand(n..m)
+  #   sleep_time = 3
+
+  #   get_post_id(board, sleep_every, sleep_time, prev_day)
+  #   get_post_content(sleep_every, sleep_time)
+  #   get_post_comment(sleep_every, sleep_time)
+  #   `python3 lib/tasks/Crawler/Dcard/data_cleaning.py`
+  #   csv_to_psql()
+  #   mv_files(table_title)
+  # end
+  # update_boards()
 end
--- lib/tasks/dcard_crawler.rake
+++ lib/tasks/dcard_crawler.rake
@@ -3,8 +3,8 @@ require("#{Rails.root}/lib/tasks/Crawler/Dcard/main.rb")
 namespace :Crawler do
   desc "Crawler for Dcard"
   task :dcard => :environment do
-    get_forums()
-    board_folder()
+    # get_forums()
+    # board_folder()
     loop_crawler()
   end
 end
