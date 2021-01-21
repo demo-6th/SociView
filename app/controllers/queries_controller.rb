9eb2d355b3b3ef279d4bbd96becea74f8761ef77
Fish Kuo <fishkuo0101@gmail.com>
2021-01-22 02:29:20 +0800

    add term frequency analysis function

--- app/controllers/queries_controller.rb
+++ app/controllers/queries_controller.rb
@@ -229,7 +229,6 @@ class QueriesController < ApplicationController
   def wordcloud; end
 
   def cloudpost
-    # pass value down to api action
     @theme = params[:theme]
     @source = [params[:dcard], params[:ptt]].delete_if { |x| x == nil }
     @start = params[:start].to_date
@@ -266,71 +265,36 @@ class QueriesController < ApplicationController
   def termfreqpost
     @theme = params[:theme]
     @source = [params[:dcard], params[:ptt]].delete_if { |x| x == nil }
-    @start = params[:start].to_s
-    @end = params[:end].to_s
+    @start = params[:start].to_date
+    @end = params[:end].to_date
     @type = [params[:post], params[:comment]].delete_if { |x| x == nil }
-    
-    if params[:dcard] && params[:ptt] #同時搜尋Dcard & PTT
-      if params[:post] && params[:comment] #同時找Post & Comment
-        @posts = Post.ransack(title_or_content_cont_any: @theme, created_at_gteq_any: @start_time, created_at_lteq_any: @end_time ).result.sort_by{|x| x[:created_at]}
-        @post_comment = Comment.ransack(post_title_or_post_content_cont_any: @theme, created_at_gteq_any: @start_time, created_at_lteq_any: @end_time).result
-        @comments = Comment.ransack(content_cont_any: @theme, created_at_gteq_any: @start_time, created_at_lteq_any: @end_time ).result
-        @comment_total = @post_comment + @comments
-        @comment_total = @comment_total.uniq.sort_by{|x| x[:created_at]}
-        @count = @posts.count + @comment_total.count
-      elsif params[:post] && !params[:comment]  #只找Post
-        @posts = Post.ransack(title_or_content_cont_any: @theme, created_at_gteq_any: @start_time, created_at_lteq_any: @end_time ).result.sort_by{|x| x[:created_at]}
-        @count = @posts.count
-      else #只找Comment
-        @post_comment = Comment.ransack(post_title_or_post_content_cont_any: @theme, created_at_gteq_any: @start_time, created_at_lteq_any: @end_time).result
-        @comments = Comment.ransack(content_cont_any: @theme, created_at_gteq_any: @start_time, created_at_lteq_any: @end_time ).result
-        @comment_total = @post_comment + @comments
-        @comment_total = @comment_total.uniq.sort_by{|x| x[:created_at]}
-        @count = @comment_total.count
-      end
-    elsif params[:dcard] && !params[:ptt] #只找Dcard
-      @source_id = 1
-      if params[:post] && params[:comment] #同時找Post & Comment
-        @posts = Post.ransack(title_or_content_cont_any: @theme, created_at_gteq_any: @start_time, created_at_lteq_any: @end_time ,board_source_id_eq: @source_id ).result.sort_by{|x| x[:created_at]}
-        @post_comment = Comment.ransack(post_title_or_post_content_cont_any: @theme, created_at_gteq_any: @start_time, created_at_lteq_any: @end_time,post_board_source_id_eq: @source_id).result
-        @comments = Comment.ransack(content_cont_any: @theme, created_at_gteq_any: @start_time, created_at_lteq_any: @end_time,post_board_source_id_eq: @source_id).result
-        @comment_total = @post_comment + @comments
-        @comment_total = @comment_total.uniq.sort_by{|x| x[:created_at]}
-        @count = @posts.count + @comment_total.count
-      elsif params[:post] && !params[:comment]  #只找Post
-        @posts = Post.ransack(title_or_content_cont_any: @theme, created_at_gteq_any: @start_time, created_at_lteq_any: @end_time ,board_source_id_eq: @source_id ).result.sort_by{|x| x[:created_at]}
-        @count = @posts.count
-      else  #只找Comment
-        @post_comment = Comment.ransack(post_title_or_post_content_cont_any: @theme, created_at_gteq_any: @start_time, created_at_lteq_any: @end_time,post_board_source_id_eq: @source_id).result
-        @comments = Comment.ransack(content_cont_any: @theme, created_at_gteq_any: @start_time, created_at_lteq_any: @end_time,post_board_source_id_eq: @source_id).result
-        @comment_total = @post_comment + @comments
-        @comment_total = @comment_total.uniq.sort_by{|x| x[:created_at]}
-        @count = @comment_total.count
-      end
-    else #只找PTT
-      @source_id = 2
-      if params[:post] && params[:comment] #同時找Post & Comment
-        @posts = Post.ransack(title_or_content_cont_any: @theme, created_at_gteq_any: @start_time, created_at_lteq_any: @end_time ,board_source_id_eq: @source_id ).result.sort_by{|x| x[:created_at]}
-        @post_comment = Comment.ransack(post_title_or_post_content_cont_any: @theme, created_at_gteq_any: @start_time, created_at_lteq_any: @end_time,post_board_source_id_eq: @source_id).result
-        @comments = Comment.ransack(content_cont_any: @theme, created_at_gteq_any: @start_time, created_at_lteq_any: @end_time,post_board_source_id_eq: @source_id).result
-        @comment_total = @post_comment + @comments
-        @comment_total = @comment_total.uniq.sort_by{|x| x[:created_at]}
-        @count = @posts.count + @comment_total.count
-      elsif params[:post] && !params[:comment]  #只找Post
-        @posts = Post.ransack(title_or_content_cont_any: @theme, created_at_gteq_any: @start_time, created_at_lteq_any: @end_time ,board_source_id_eq: @source_id ).result.sort_by{|x| x[:created_at]}
-        @count = @posts.count
-      else  #只找Comment
-        @post_comment = Comment.ransack(post_title_or_post_content_cont_any: @theme, created_at_gteq_any: @start_time, created_at_lteq_any: @end_time,post_board_source_id_eq: @source_id).result
-        @comments = Comment.ransack(content_cont_any: @theme, created_at_gteq_any: @start_time, created_at_lteq_any: @end_time,post_board_source_id_eq: @source_id).result
-        @comment_total = @post_comment + @comments
-        @comment_total = @comment_total.uniq.sort_by{|x| x[:created_at]}
-        @count = @comment_total.count
-      end
+
+    post_result = Post.where("created_at >= ? and created_at <= ?", @start.midnight, @end.end_of_day).where("content like ? or title like ?", "%#{@theme}%", "%#{@theme}%")
+    comment_result = Comment.where("created_at >= ? and created_at <=?", @start.midnight, @end.end_of_day).where(:pid => Post.where("content like ? or title like ?", "%#{@theme}%", "%#{@theme}%").pluck(:pid)).or(Comment.where("created_at >= ? and created_at <=?", @start.midnight, @end.end_of_day).where("content like ?", "%#{@theme}%"))
+
+    post_count = post_result.count
+    comment_count = comment_result.count
+
+    if params[:post] && params[:comment]
+      @count = post_count + comment_count
+      result = post_result.select(:token, :pos) | comment_result.select(:token, :pos)
+    elsif params[:post] && !params[:comment]
+      @count = post_count
+      result = post_result.select(:token, :pos)
+    else
+      @count = comment_count
+      result = comment_result.select(:token, :pos)
+    end
+
+    CSV.open("data/tf_data.csv", "wb") do |csv|
+      result.find_all do |res|
+        csv << res.attributes.values
+      end 
+      @termfreq = `python3 lib/tasks/Termfreq/main.py params`
     end
   end
 
   private
-
   def search_post_only(query)
     @posts = Post.search query, fields: [:title, :content], misspellings: false, where: { created_at: { gte: @start, lte: @end } }, order: { created_at: { order: "asc" } }
   end
---
+++ lib/tasks/Termfreq/main.py
@@ -0,0 +1,47 @@
+# coding=utf-8
+import pandas as pd
+import ast
+from matplotlib import pyplot as plt
+import os
+from collections import Counter
+import csv
+
+pd.options.mode.chained_assignment = None
+
+data = pd.read_csv("data/tf_data.csv",names=["token", "pos"])
+
+data = data.dropna()
+data.reset_index(inplace=True)
+data = data.drop(['index'], axis=1)
+
+for i in range(len(data)):
+  try:
+    data["token"][i] = ast.literal_eval(data["token"][i])
+    data["pos"][i] = ast.literal_eval(data["pos"][i])
+  except:
+    continue
+
+with open("data/dict/stopwords.txt", encoding="utf-8") as fin:
+    stopwords = fin.read().split("\n")[1:]
+
+# V, N, A
+def pos_counter(data, pos_type):
+  result = []
+
+  for i in range(len(data)):
+    for n in range(len(data.pos[i])):
+      if data.pos[i][n].startswith(pos_type) and data.token[i][n] not in stopwords:
+        result.append(data.token[i][n])
+    count = Counter(result).most_common(20)
+
+  with open('data/tf_'+pos_type+'.csv','w') as csvfile:
+    writer=csv.writer(csvfile)
+    writer.writerows(count)
+
+
+if len(data) < 50:
+  print("您所選擇區間資料過少，請重新選擇")
+else:
+  pos_counter(data, "V")
+  pos_counter(data, "A")
+  pos_counter(data, "N")
