9990dca59671b954021726134985e8dac4fd2c71
mino <oil_chen@cycu.org.tw>
2021-01-22 15:26:15 +0800

    add css

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   rescue_from ActiveRecord::RecordNotFound, with: :not_found_page
+
   
   def not_found_page
     render plain: "404 Not Found",
