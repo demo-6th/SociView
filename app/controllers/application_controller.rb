1bce127b614463399736a93a13a3e32d190e7c42
mino <oil_chen@cycu.org.tw>
2021-01-25 17:59:52 +0800

    打開第三方登入

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   rescue_from ActiveRecord::RecordNotFound, with: :not_found_page
-
+  # skip_before_action :verify_authenticity_token
   
   def not_found_page
     render plain: "404 Not Found",
