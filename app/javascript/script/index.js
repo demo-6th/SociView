4a3d90326168243cc91c50aef37e810b17488fe9
mino <oil_chen@cycu.org.tw>
2021-01-25 17:24:46 +0800

    暫停ajax

--- app/javascript/script/index.js
+++ app/javascript/script/index.js
@@ -1,7 +1,11 @@
 import "./checkbox.js";
 import "./datepicker.js";
-import "./volume_ajax.js";
-import "./sentiment_ajax.js";
+// import "./volume_ajax.js";
+// import "./sentiment_ajax.js";
 import "./termfreq.js";
 import "./listpost.js"
+import "./sentiment.js";
+import "./volume.js";
+
+
 // import "./tab_click.js"
