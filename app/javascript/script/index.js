b04ddab6e95a75f0297d2c1bdd962183cb066b25
dante <dantecherng@hotmail.com.tw>
2021-01-26 23:58:31 +0800

    fix sourcebar

--- app/javascript/script/index.js
+++ app/javascript/script/index.js
@@ -2,9 +2,9 @@ import "./checkbox.js";
 import "./datepicker.js";
 // import "./volume_ajax.js";
 // import "./sentiment_ajax.js";
+// import "./tab_click.js"
 import "./termfreq.js";
 import "./listpost.js"
 import "./sentiment.js";
 import "./volume.js";
-
-// import "./tab_click.js"
+import "./source.js";
