1435651b9cf181bc4f52d870f86c775d443671ea
dante <dantecherng@hotmail.com.tw>
2021-01-26 21:34:22 +0800

    done

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -19,7 +19,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   # PUT /resource
   def update
-    redirect_to list_queries_path, notice: "修改成功!"
+    redirect_to volume_queries_path, notice: "修改成功!"
   end
 
   # DELETE /resource
@@ -50,7 +50,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   # The path used after sign up.
   def after_sign_up_path_for(resource)
-    list_queries_path
+    volume_queries_path
   end
 
   # The path used after sign up for inactive accounts.
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,10 @@
 require("#{Rails.root}/lib/tasks/Crawler/Dcard/dcard_process_files.rb")
+require("#{Rails.root}/lib/tasks/Crawler/PTT/ptt_process_files.rb")
 
 Source.create(name: "Dcard")
 Source.create(name: "PTT")
-Source.create(name: "Eney")
 
-# update_testboards()
-# testcsv_to_psql()
+# dcard_boards()
+# dcard_csv_to_psql()
+ptt_boards()
+ptt_csv_to_psql()
