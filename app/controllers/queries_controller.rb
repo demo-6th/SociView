1efac397299ffbaf4e4d82033a00cee9deabf4cb
Fish Kuo <fishkuo0101@gmail.com>
2021-01-28 12:00:43 +0800

    edit keywords

--- app/controllers/queries_controller.rb
+++ app/controllers/queries_controller.rb
@@ -251,6 +251,29 @@ def search_box
   @type = [params[:post], params[:comment]].delete_if { |x| x == nil }
 end
 
+def check_search_box
+  @theme = [params[:theme1], params[:theme2], params[:theme3_input]].delete_if { |x| x == nil }
+  @source = [params[:dcard], params[:ptt]].delete_if { |x| x == nil }
+  @start = params[:start].to_date
+  @end = params[:end].to_date
+  @type = [params[:post], params[:comment]].delete_if { |x| x == nil }
+end
+
+# topic keywords array
+def theme_keywords(theme)
+  result = []
+  if theme == "萊豬"
+    # result = ["萊豬", "萊克多巴胺", "萊牛", "瘦肉精", "食安", "受體素", "美國國會", "副作用", "美國FDA", "CODEX", "聯合國國際食品法典委員會", "容許量", "食品安全衛生管理法", "萊劑", "AIT", "藥物", "毒豬", "毒牛", "溫體", "冷凍豬肉", "殘留"]
+    result = ["萊豬", "萊克多巴胺", "毒豬"]
+  elsif theme == "新冠肺炎"
+    # result = ["口罩", "武漢", "陳時中", "鋼鐵部長", "誠實中", "疾病管制署", "Covid", "傳染", "疫情", "防疫", "肺炎", "感染", "疫苗", "疫情指揮中心", "張上淳", "陳宗彥", "周志浩", "莊人祥", "1922", "疾管", "本土案例", "境外案例", "病例", "偽出國", "變種病毒", "瘟疫", "疫調", "病毒", "染疫", "自主健康管理", "隔離", "隔離檢疫", "居家隔離", "居家檢疫", "中國武肺", "味覺喪失", "嗅覺喪失", "採檢", "CT值", "超前部署", "新冠疫苗", "無症狀", "境外移入", "確診", "敦陸艦隊"]
+    result = ["武漢肺炎", "武肺", "新冠肺炎", "疫情"]
+  else
+    result = [theme]
+  end
+  return result
+end
+
 # topic keywords array
 def theme_keywords(theme)
   result = []
