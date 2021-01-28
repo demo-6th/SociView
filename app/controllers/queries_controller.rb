af0e2c48bbcc8fc48d3b4c750df12c49405546b6
Ryan Lee <ryanlee.coding@gmail.com>
2021-01-28 15:26:03 +0800

    per(50)

--- app/controllers/queries_controller.rb
+++ app/controllers/queries_controller.rb
@@ -41,9 +41,9 @@ class QueriesController < ApplicationController
     end
 
     if params[:sort] == '由新到舊' || params[:sort] == '由舊到新'
-      @results = @result.order(created_at: @sort).page(params[:page]).per(20)
+      @results = @result.order(created_at: @sort).page(params[:page]).per(50)
     else
-      @results = @result.order(like_count: @sort).page(params[:page]).per(20)
+      @results = @result.order(like_count: @sort).page(params[:page]).per(50)
     end
    @count = @results.total_count
   end
