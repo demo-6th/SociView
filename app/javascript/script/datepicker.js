fb1708084496a218aaa4522153b32c92f6df346d
Fish Kuo <fishkuo0101@gmail.com>
2021-01-28 11:08:14 +0800

    add date range

--- app/javascript/script/datepicker.js
+++ app/javascript/script/datepicker.js
@@ -11,9 +11,16 @@ document.addEventListener("turbolinks:load", () => {
   //   }
   // })
   startPicker = flatpickr(".datepicker_start", {
-    onChange: function(selectedDates, dateStr, instance) {
-      endPicker.set('minDate', selectedDates[0]);
-    }
+    enable: [
+      {
+        from: "2021-01-14",
+        to: "2021-01-21",
+      },
+    ],
+    onChange: function (selectedDates, dateStr, instance) {
+      endPicker.set("minDate", selectedDates[0]);
+      endPicker.set("maxDate", "2021-01-21");
+    },
   });
   endPicker = flatpickr(".datepicker_end", {});
-})
+});
