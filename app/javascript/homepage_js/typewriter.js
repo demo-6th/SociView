ad790947a1aa0705e38b8b3ba4c15cb8f7b84648
mino <oil_chen@cycu.org.tw>
2021-01-29 09:04:12 +0800

    跑馬燈速度增加一點

--- app/javascript/homepage_js/typewriter.js
+++ app/javascript/homepage_js/typewriter.js
@@ -1,12 +1,12 @@
 document.addEventListener("turbolinks:load", () => {
     const dom = document.querySelector('#typewriter')
-    const data = "　　　　　　　　　　　　　　　　　　　　您好，本網頁適用解析度為　１０２４　x　７６８　以上，低於此規格會降低使用者體驗，敬請見諒　　　　　　　　　　　　　　　　　　　　".split('')
+    const data = "　　　　　　　　　　　　　　　　　　　　您好，本網頁適用解析度為　1024 x 786 以上，低於此規格會降低使用者體驗，敬請見諒　　　　　　　　　　　　　　　　　　　　".split('')
     let index = 0
 
     function writing(index) {
         if (index < data.length - 19) {
             dom.innerHTML = data.slice(index, index + 21).join('')
-            setTimeout(writing.bind(this), 300, ++index)
+            setTimeout(writing.bind(this), 200, ++index)
         } else {
             index = 0
             writing(index)
